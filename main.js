1c78ba940b01d20f45b122edb114866ba7baf094
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-03 19:06:47 +0200

    Cambios en espaciado y enunciados para mejor legibilidad

--- main.js
+++ main.js
@@ -2,14 +2,20 @@
     // Conversió de funcions: Tens una funció add que accepta dos paràmetres i retorna la seva suma.
     // Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
 
-const add = (a, b) => a + b;
+    const add = (a, b) => a + b;
+
+
+
 
 
 // EXERCICI 2
     // Funció de fletxa sense paràmetres:
     // Crea una funció de fletxa anomenada randomNumber que no necessiti paràmetres i que retorni un número aleatori entre 0 i 100.
 
-const randomNumber = () => Math.floor(Math.random() * 101);
+    const randomNumber = () => Math.floor(Math.random() * 101);
+
+
+
 
 
 // EXERCICI 3
@@ -17,42 +23,50 @@ const randomNumber = () => Math.floor(Math.random() * 101);
     // Crea una classe person que tingui una propietat name i una funció greet que utilitzi una funció de fletxa.
     // La funció ha d'imprimir una salutació que inclogui el nom de la persona. Per exemple: console.log(Hola, ${this.name});.
 
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
+    class Person {
+    constructor(name) {
+        this.name = name;
+    }
 
-  greet = () => {
-    console.log(`Hola, ${this.name}`);
-  }
-}
+    greet = () => {
+        console.log(`Hola, ${this.name}`);
+    }
+    }
 
-const person1 = new Person("Arnau");
-person1.greet();
+    const person1 = new Person("Arnau");
+    person1.greet();
+
+
+
+
 
 // EXERCICI 4
     // Funció de fletxa dins d'un loop:
     // Crea una funció anomenada printNumbers que accepti un array de números i utilitzi un loop for per imprimir cada número a la consola utilitzant una funció de fletxa.
 
-const printNumbers = (numbers) => {
-  numbers.forEach(number => console.log(number));
-};
+    const printNumbers = (numbers) => {
+    numbers.forEach(number => console.log(number));
+    };
 
 
-printNumbers([1, 2, 3, 4, 5]);
+    printNumbers([1, 2, 3, 4, 5]);
+
+
+
+
 
 // EXERCICI 5
     // Funció de fletxa amb 'setTimeout':
     // Crea una funció de fletxa que imprimeixi un missatge a la consola després d'esperar 3 segons.
 
-const delayedMessage = () => {
-  setTimeout(() => {
-    console.log("Missatge imprès després de 3 segons!");
-  }, 3000);
-};
+    const delayedMessage = () => {
+    setTimeout(() => {
+        console.log("Missatge imprès després de 3 segons!");
+    }, 3000);
+    };
 
 
-delayedMessage();
+    delayedMessage();
 
 
 
