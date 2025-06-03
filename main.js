52c09af02fb658f830879002e256b5eff684757d
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-03 18:32:45 +0200

    Adición de los enunciados de cada ejercicio como comentarios

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-// EXERCICI 1
+// EXERCICI 1 
+    // Conversió de funcions: Tens una funció add que accepta dos paràmetres i retorna la seva suma.
+    // Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
 
 const add = (a, b) => a + b;
 
 
 // EXERCICI 2
+    // Funció de fletxa sense paràmetres:
+    // Crea una funció de fletxa anomenada randomNumber que no necessiti paràmetres i que retorni un número aleatori entre 0 i 100.
 
 const randomNumber = () => Math.floor(Math.random() * 101);
 
 
 // EXERCICI 3
+    // Ús de 'this' en les funcions de fletxa:
+    // Crea una classe person que tingui una propietat name i una funció greet que utilitzi una funció de fletxa.
+    // La funció ha d'imprimir una salutació que inclogui el nom de la persona. Per exemple: console.log(Hola, ${this.name});.
 
 class Person {
   constructor(name) {
@@ -24,6 +31,8 @@ const person1 = new Person("Arnau");
 person1.greet();
 
 // EXERCICI 4
+    // Funció de fletxa dins d'un loop:
+    // Crea una funció anomenada printNumbers que accepti un array de números i utilitzi un loop for per imprimir cada número a la consola utilitzant una funció de fletxa.
 
 const printNumbers = (numbers) => {
   numbers.forEach(number => console.log(number));
@@ -33,6 +42,8 @@ const printNumbers = (numbers) => {
 printNumbers([1, 2, 3, 4, 5]);
 
 // EXERCICI 5
+    // Funció de fletxa amb 'setTimeout':
+    // Crea una funció de fletxa que imprimeixi un missatge a la consola després d'esperar 3 segons.
 
 const delayedMessage = () => {
   setTimeout(() => {
@@ -42,5 +53,7 @@ const delayedMessage = () => {
 
 
 delayedMessage();
+
+
 
 
