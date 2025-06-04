7f3057b4e4c75cab7fc32b2fd23565f34a9d5eac
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-04 17:48:13 +0200

    Ejercicio 4 (Funciones + console logs de resultados)

--- main.js
+++ main.js
@@ -2,16 +2,12 @@
     // Map: Teniu un array de números [1, 2, 3, 4].
     // Crea una nova array que contingui el quadrat de cada número.
 
-        
     const array1 = [1, 2, 3, 4];
     const squareArray1 = array1.map(num => num * num);
 
     console.log(`Array inicial: `, array1);
     console.log('Array de quadrats: ', squareArray1);
     
-
-
-
 // EXERCICI 2
     // Filter: Teniu una array de números [1, 2, 3, 4].
     // Crea una nova array que només contingui els números parells.
@@ -22,14 +18,15 @@
     console.log(`Array inicial: `, array2);
     console.log(`Array de números parells: ` , evenArray2);
 
-
-
-
-
 // EXERCICI 3
     // Find: Teniu una array de números [1, 10 , 8, 11].
     // Utilitza la funció find per a trobar el primer número que és major a 10.
 
+    const array3 = [1, 10, 8, 11];
+    const numberBiggerThan10 = array3.find(num => num > 10);
+
+    console.log(`Array inicial: `, array3);
+    console.log('Primer número major que 10:', numberBiggerThan10);
 
 
 
