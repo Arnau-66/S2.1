8e9dd36db8bdef6d73a2ef945a78951438718c18
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-04 18:58:56 +0200

    Ejercicio 5 (Funciones + console logs de resultados)

--- main.js
+++ main.js
@@ -45,9 +45,11 @@
     // - Calcula la suma dels nombres filtrats i multiplicats per 2.
     // - La funció ha de retornar el resultat de la suma.
 
-    const finalResult = arr => arr.filter(num => num >= 10).map(num => num * 2).reduce((acc, num) => acc + num, 0);
+    const array5 = [1, 3, 7, 10, 15, 17, 11, 5, 8, 12, 9];
+    const finalResult = array5 => array5.filter(num => num >= 10).map(num => num * 2).reduce((count, num) => count + num, 0);
 
-    console.log(finalResult([1, 3, 7, 10, 15, 17, 11, 5, 8, 12, 9]));
+    console.log(`Array inicial: `, array5);
+    console.log(`Resultat final: ${finalResult(array5)}`);
 
 
     
