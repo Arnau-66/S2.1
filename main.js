336bf1fee7d5908b88483dc52087090939fbeab4
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-04 18:37:58 +0200

    Ejercicio 4 + enunciado ejercicio 5

--- main.js
+++ main.js
@@ -31,6 +31,20 @@
 // EXERCICI 4
     // Reduce: Teniu una array de números [13, 7, 8, 21].
     // Fes servir la funció reduce per a calcular la suma total dels números.
+
+    const array4 = [13, 7, 8, 21];
+    const totalArray = array4.reduce((count, num) => count + num, 0);
+
+    console.log('Array inicial: ', array4);
+    console.log('Suma total:', totalArray);
+
+// EXERCICI 5
+    // Donat un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", crea una funció en una sola línia que faci el següent:
+    // - Filtra els nombres majors o iguals a 10.
+    // - Multiplica cada nombre filtrat per 2.
+    // - Calcula la suma dels nombres filtrats i multiplicats per 2.
+    // - La funció ha de retornar el resultat de la suma.
+
     
 
 
