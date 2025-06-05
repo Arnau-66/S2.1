664890cd7561e7ee55d5e09f0b9d2c928561316d
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-05 10:37:21 +0200

    Enunciado ejercicio 6

--- main.js
+++ main.js
@@ -71,6 +71,11 @@
             }
         }
 
+// EXERCICI 6
+    // for-of amb index:
+    // Utilitza un bucle for-of per a imprimir a la consola cada element de l'array i la seva posició (index):
+    // let noms = ['Anna', 'Bernat', 'Clara']
+
 
 
 
