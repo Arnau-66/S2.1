6854d5d97cfee60f59c7ccbae75d66075f8758ac
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-05 10:51:43 +0200

    Enunciado ejercicio 2

--- main.js
+++ main.js
@@ -7,6 +7,11 @@
                 resolve(console.log('Hola, món'));
             }, 2000);
         });
+
+// EXERCICI 2
+    // Utilització d'una Promesa:
+    // Utilitza la promesa creada en l'exercici anterior.
+    // Crea un .then que imprimeixi el resultat a la consola.
     
             
 
