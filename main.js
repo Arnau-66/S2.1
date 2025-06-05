8f8f9c119f51ab5c7562965f2ff8f15c74b156c2
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-05 11:24:42 +0200

    Enunciado ejercicio 5

--- main.js
+++ main.js
@@ -52,6 +52,10 @@
 
     printResult();
 
+// EXERCICI 5
+    // Gestió d'errors amb async/await:
+    // Modifica la funció de l'exercici 4 per a que capturi qualsevol possible error utilitzant un bloc try/catch.
+
 
 
 
