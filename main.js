60cc3266a40a2687bee3c0f561151688478a1bc2
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-05 10:49:28 +0200

    Ejercicio 1 (Funciones + console logs de resultados)

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
     // Creació d'una Promesa:
     // Crea una promesa que es resolgui després de 2 segons i que retorni la cadena de text 'Hola, món'.
 
+    const promise1 = new Promise((resolve) => {
+        setTimeout(() => {
+                resolve(console.log('Hola, món'));
+            }, 2000);
+        });
     
-        
+            
 
 
 
