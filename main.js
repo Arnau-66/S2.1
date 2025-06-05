073d9401fe7e137f70d26c4192d7c1a59c5654da
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-05 10:21:08 +0200

    Enunciado ejercicio 4

--- main.js
+++ main.js
@@ -34,6 +34,11 @@
     console.log(`Array incial: `, numeros);
     console.log('Números parells:', evenNumbers);
 
+// Exercici 4
+    // for-in: Teniu un objecte amb parells clau-valor:
+    // let obj = { nom: Ona, edat: 25, ciutat: 'Barcelona' };
+    // Utilitza un bucle for-in per a imprimir a la consola cada clau i el seu valor corresponent.
+
 
 
 
