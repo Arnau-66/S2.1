67c50fe1e0adbd0e940380da785ba253c0ec4e6a
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-05 10:17:30 +0200

    Enunciado ejercicio 3

--- main.js
+++ main.js
@@ -12,7 +12,6 @@
         });
 
 // EXERCICI 2
-    
     // for-of: Teniu una array de noms.
     // Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
 
@@ -23,7 +22,10 @@
         console.log(name);
         }
 
-
+// Exercici 3
+    // filter: Teniu una array de números.
+    // Utilitza filter per a crear una nova array que només contingui els números parells.
+    // let numeros = [1, 2, 3, 4, 5, 6];
 
     
 
