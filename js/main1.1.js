833c52249806d192ec5d6e2b5ff65766ffb06310
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-16 17:32:24 +0200

    Merging main1.1 from 1.1 branch

--- js/main1.1.js
+++ js/main1.1.js
@@ -1,123 +1,62 @@
 // EXERCICI 1 
-    // Creació d'una Promesa:
-    // Crea una promesa que es resolgui després de 2 segons i que retorni la cadena de text 'Hola, món'.
+    // Conversió de funcions: Tens una funció add que accepta dos paràmetres i retorna la seva suma.
+    // Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
 
-    const promise1 = new Promise((resolve) => {
-        setTimeout(() => {
-                resolve('Hola, món');
-            }, 2000);
-        });
+    const add = (a, b) => a + b;
+
+    
+
+
 
 // EXERCICI 2
-    // Utilització d'una Promesa:
-    // Utilitza la promesa creada en l'exercici anterior.
-    // Crea un .then que imprimeixi el resultat a la consola.
-    
-    promise1.then(result=> {
-        console.log(`Resultat exercici 2: `);
-        console.log(result);
-        });
+    // Funció de fletxa sense paràmetres:
+    // Crea una funció de fletxa anomenada randomNumber que no necessiti paràmetres i que retorni un número aleatori entre 0 i 100.
+
+    const randomNumber = () => Math.floor(Math.random() * 101);
+    const result = randomNumber();
+    console.log(`Número aleatori: ${result}`);
 
 // EXERCICI 3
-    // Promesa amb reject:
-    // Crea una promesa que es resolgui després de 2 segons si l'input és igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa.
+    // Ús de 'this' en les funcions de fletxa:
+    // Crea una classe person que tingui una propietat name i una funció greet que utilitzi una funció de fletxa.
+    // La funció ha d'imprimir una salutació que inclogui el nom de la persona. Per exemple: console.log(Hola, ${this.name});.
 
-    const checkWord = (input) => {
-        return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (input === 'Hola') {
-                            resolve(`Input correcte`);
-                        } else {
-                            reject(`Input incorrecte`);
-                        }
-                    }, 2000);
-            });
-        };
+    class Person {
+    constructor(name) {
+        this.name = name;
+    }
 
-    checkWord('Hola')
-    .then(result => console.log(`Hola: `, result));
+    greet = () => {
+        console.log(`Hola, ${this.name}`);
+    }
+    }
 
-    checkWord('Adeu')
-    .catch(error => console.log(`Adeu: `, error));
+    const person1 = new Person("Arnau");
+    person1.greet();
 
 // EXERCICI 4
-    // Ús de async/await:
-    // Escriu una funció asíncrona que utilitzi la funció await per a esperar el resultat de la promesa creada a l'exercici 1
-    // i que després imprimeixi aquest resultat a la consola.
+    // Funció de fletxa dins d'un loop:
+    // Crea una funció anomenada printNumbers que accepti un array de números i utilitzi un loop for per imprimir cada número a la consola utilitzant una funció de fletxa.
 
-    const printResult = async () => {
-        const result = await promise1;
-        console.log(`Resultat exercici 4: `, result);
+    const printNumbers = (numbers) => {
+        numbers.forEach(number => console.log(number));
         };
-
-    printResult();
+    
+    console.log(`Print numbers: `);
+    printNumbers([1, 2, 3, 4, 5]);
 
 // EXERCICI 5
-    // Gestió d'errors amb async/await:
-    // Modifica la funció de l'exercici 4 per a que capturi qualsevol possible error utilitzant un bloc try/catch.
-
-    const validMessage = `Hola, món`;
-    const invalidMessage = `Adeu, món`;
-
-    const checkInputs5 = (input) => {
-        return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    if (input === validMessage) {
-                        resolve('Input correcte');
-                        } else {
-                                reject('Input incorrecte');
-                            }
-                }, 2000);
-        });
+    // Funció de fletxa amb 'setTimeout':
+    // Crea una funció de fletxa que imprimeixi un missatge a la consola després d'esperar 3 segons.
+    
+    const delayedMessage = () => {
+    setTimeout(() => {
+        console.log("Missatge imprès després de 3 segons!");
+    }, 3000);
     };
 
-    const checkPromise5 = async (input) => {
-    try {
-        const result = await checkInputs5(input);
-        console.log('Hola, món: ', result);
-
-    } catch (error) {
-        console.log(`Adeu, món: `, error);
-    }
-    };
-
-    checkPromise5(validMessage);
-    checkPromise5(invalidMessage);
-
-// EXERCICI 6
-    // Promise.all:
-    // Crea dues promeses que es resolguin després de 2 i 3 segons, respectivament.
-    // Utilitza Promise.all per a esperar que ambdues promeses es resolguin, i imprimeix els resultats a la consola.
-
-    const promise6 = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(`Resultat de la promesa 1 (2 segons)`);
-        }, 2000);
-    });
-
-  
-    const promise7 = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(`Resultat de la promesa 2 (3 segons)`);
-        }, 3000);
-    });
-
-    Promise.all([promise6, promise7])
-        .then(resultats => {
-            console.log(`Totes les promeses s\'han resolt: `);
-            console.log(resultats);
-        })
-        .catch(error => {
-            console.log(`Hi ha hagut un error: `, error);
-        });
-
-
-
-
-
-
-
-
+    console.log(`Espera 3 segons:`);
+    delayedMessage();
 
 
 
